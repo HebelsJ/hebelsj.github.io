640b5068bddc5e5a733d2feea6a6e930a9189067
HebelsJ <hebelsj@gmail.com>
2018-08-23 11:24:03 -0700

    js test

---
+++ cpguide1.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+var i;
+for (i = 0; i < 800; i++) { 
+    cp.hide("icon_" + i);
+    cp.hide("task_" + i);
+    cp.hide("Box_" + i);
+    cp.hide("hide_" + i);
+    cp.show("show_" + i);
+}
+</script>
